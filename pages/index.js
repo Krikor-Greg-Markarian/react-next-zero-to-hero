2cc5a74fb637e54d8ff4e391b273f7129b09dbb6
Greg <greg_markarian@hotmail.com>
2022-02-16 09:55:16 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>React next zero to hero</title>
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
