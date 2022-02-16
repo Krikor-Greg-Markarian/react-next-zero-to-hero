afd2c6c570346f4d20fd945f69237379014fa65b
Greg <greg_markarian@hotmail.com>
2022-02-16 11:06:12 +0200

    WIP

--- pages/exercise1.js
+++ pages/exercise1.js
@@ -1,10 +1,26 @@
-import React from "react";
 import ComponentA from "../components/ComponentA";
 import cn from "classnames";
+import React, { useState } from "react";
 const exercise1 = () => {
+  const [isClicked, setIsClicked] = useState(false);
   return (
-    <div className={cn("flex justify-center items-center")}>
-      <ComponentA />
+    <div>
+      <div className={cn("flex justify-center items-center")}>
+        <ComponentA
+          buttonName='Click Me'
+          onClick={() => {
+            alert("Hello World!");
+          }}
+        />
+      </div>
+
+      <div className={cn("flex justify-center items-center pt-2")}>
+        <ComponentA
+          buttonName='Click Me'
+          onClick={() => setIsClicked(true)}
+          isSelected={isClicked}
+        />
+      </div>
     </div>
   );
 };
